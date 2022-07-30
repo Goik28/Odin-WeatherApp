bfb87e85859320b5dd2790a5e5d520afbcb0d2e5
Goik <sbabinski@outlook.com>
2022-07-30 13:59:18 -0300

    Init webpack modules, eslint and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
+/* eslint-disable no-undef */
 const path = require("path");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'WeatherApp',
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
   },
 };
